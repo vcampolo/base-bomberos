908225f408373ae31afcca3ed54c8c97b9b9119e
vcampolo <128171564+vcampolo@users.noreply.github.com>
2025-10-30 12:57:48 -0300

    Cambios guardados con exito

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", fontSize: "24px" }}>
+      Base Bomberos 🚒 — Bienvenido
+    </div>
+  );
+}
